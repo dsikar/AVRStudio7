430fc7e37a6a2fceee2043d0339bf2318cf7e6de
Daniel Sikar <dsikar@tycoint.com>
2017-09-13 14:41:56 +0100

    Blink example imported into AVR Studio 7

---
+++ BlinkSketch/BlinkSketch/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 11/09/2017 10:58:37
+ * Author : dsikar
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
